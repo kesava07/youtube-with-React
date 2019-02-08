2cfbdcd848eae12e2ac211c39ab5eb70ee412149
kesava07 <kesava8148420073@gmail.com>
2019-02-08 05:28:48 -0800

    google maps

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -4,6 +4,7 @@ import SearchBar from '../Components/SearchBar';
 import youtube from '../Api/Youtube'
 import VideoDeatils from '../Components/VideoDeatils';
 import VideosList from '../Components/VideoList';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class App extends Component {
   state = {
@@ -20,7 +21,7 @@ class App extends Component {
   }
   handleselect = (video) => {
     this.setState({ selectedVideo: video })
-    console.log(this.state.selectedVideo,"chenna kesava")
+    console.log(this.state.selectedVideo, "chenna kesava")
   }
   render() {
     return (
@@ -29,10 +30,27 @@ class App extends Component {
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-8 mt-4">
-              <VideoDeatils video={this.state.selectedVideo}/>
+              <VideoDeatils video={this.state.selectedVideo} />
             </div>
             <div className="col-md-4 mt-4">
               <VideosList videos={this.state.videos} handleselect={this.handleselect} />
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <div className="card card-body mb-5">
+            <div id="map">
+              <Map
+                google={this.props.google}
+                zoom={16}
+                initialCenter={{
+                  lat:17.4399,
+                  lng: 78.4983
+                }}
+              >
+                <Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />
+              </Map>
             </div>
           </div>
         </div>
@@ -41,4 +59,6 @@ class App extends Component {
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCgZ4MSAN1Qa88ma5OWu4urlk0x4N7k-wE")
+})(App)
